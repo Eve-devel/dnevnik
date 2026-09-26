810a6dcd87fa9b1a054edfca9968e8a28fdb61c2
Eve-devel <54936286+Eve-devel@users.noreply.github.com>
2026-09-26 19:51:19 +0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Собрано автоматически, правьте build.js
-var CACHE = "dnevnik-33b70d75";
+var CACHE = "dnevnik-26d1e8f0";
 var ASSETS = [
   "./", "./index.html", "./manifest.webmanifest",
   "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png"
